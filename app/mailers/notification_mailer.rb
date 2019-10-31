6222e3572e5ae6f914f3d0bf9fb7c79bb551b250
Al Trujillo <Trujillo0525@gmail.com>
2019-10-31 15:56:35 +0000

    auto email added

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "Trujillo0525@gmail.com",
+    mail(to: "codingtester123@gmail.com",
       subject: "A comment has been added to your place")
   end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,9 +4,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com"
+    domain: "example.com",
     authentication: "plain",
-    enable_startitls_auto: true,
+    enable_starttls_auto: true,
     user_name: ENV['GMAIL_ADDRESS'],
     password: ENV['GMAIL_PASSWORD']
   }
